30fa5f6c194c01b8caba5644e578431e43c1d67b
Matteo Fattorini <fattorinimatteo@gmail.com>
2020-11-20 17:35:32 +0100

    Implemented search on contacts

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 const app = new Vue({
   el: "#app",
-    data: {
-      
+  data: {
     mySelf: {
-      avatar: "css/img/avatar_2.jpg",   //self-contact
+      avatar: "css/img/avatar_2.jpg", //self-contact
       name: "Matteo Fattorini",
     },
 
-    contactSearch : "",     //v-modeled with search in contact
-        
-    contacts: [     //array of contacts, as obj, with avatar,name,lastSeen proprieties
-       {
+    contactSearch: "", //v-modeled with search in contact
+
+    contacts: [
+      //array of contacts, as obj, with avatar,name,lastSeen proprieties
+      {
         avatar: "css/img/avatar_io.jpg",
         name: "Noemi",
         lastSeen: "",
       },
-       {
+      {
         avatar: "css/img/avatar_1.jpg",
         name: "Jacopo",
       },
-       {
+      {
         avatar: "css/img/avatar_3.jpg",
         name: "Giulio",
         lastSeen: "",
@@ -29,12 +29,12 @@ const app = new Vue({
         name: "Marco",
         lastSeen: "",
       },
-       {
+      {
         avatar: "css/img/avatar_5.jpg",
         name: "Elia",
         lastSeen: "",
       },
-       {
+      {
         avatar: "css/img/avatar_6.jpg",
         name: "Giulia",
         lastSeen: "",
@@ -44,14 +44,26 @@ const app = new Vue({
         name: "Alessio",
         lastSeen: "",
       },
-       {
+      {
         avatar: "css/img/avatar_8.jpg",
         name: "Luca",
         lastSeen: "",
       },
     ],
+    filteredContacts: [], //new array, based of search
   },
-  methods: {},
+
+  methods: {
+    /** this methos is for searching. If an element in the search bar is present in any of contact name,
+    add it to the filtered contact list. In HTML v-for is on normal contacts if search is empty and on filtered if not */
+      
+    search() {
+      this.filteredContacts = this.contacts.filter((contact, i) =>
+        contact.name.toLowerCase().includes(this.contactSearch.toLowerCase())
+      );
+    },
+    },
+  
   computed: {},
 });
 
