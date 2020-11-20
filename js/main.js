c7aa2b7802a4db837722481ff2929a14228f9534
Matteo Fattorini <fattorinimatteo@gmail.com>
2020-11-20 19:05:17 +0100

    Fixes

--- js/main.js
+++ js/main.js
@@ -14,44 +14,62 @@ const app = new Vue({
         avatar: "css/img/avatar_io.jpg",
         name: "Noemi",
         lastSeen: "",
+        lastMessage: "",
+        id: 100,
       },
       {
         avatar: "css/img/avatar_1.jpg",
         name: "Jacopo",
+        lastSeen: "",
+        lastMessage: "",
+        id: 101,
       },
       {
         avatar: "css/img/avatar_3.jpg",
         name: "Giulio",
         lastSeen: "",
+        lastMessage: "",
+        id: 102,
       },
       {
         avatar: "css/img/avatar_4.jpg",
         name: "Marco",
         lastSeen: "",
+        lastMessage: "",
+        id: 103,
       },
       {
         avatar: "css/img/avatar_5.jpg",
         name: "Elia",
         lastSeen: "",
+        lastMessage: "",
+        id: 104,
       },
       {
         avatar: "css/img/avatar_6.jpg",
         name: "Giulia",
         lastSeen: "",
+        lastMessage: "",
+        id: 105,
       },
       {
         avatar: "css/img/avatar_7.jpg",
         name: "Alessio",
         lastSeen: "",
+        lastMessage: "",
+        id: 106,
       },
       {
         avatar: "css/img/avatar_8.jpg",
         name: "Luca",
         lastSeen: "",
+        lastMessage: "",
+        id: 107,
       },
     ],
     filteredContacts: [], //new array, based of search
     searchToggle: false, // will use this to know when user tried to search something
+    currentContact: {}, //this will be the selected contacts. Chat will change on this status
   },
 
   methods: {
@@ -60,9 +78,20 @@ const app = new Vue({
 
     search() {
       this.searchToggle = true;
-      this.filteredContacts = this.contacts.filter((contact, i) =>
+      this.filteredContacts = this.contacts.filter((contact) =>
         contact.name.toLowerCase().includes(this.contactSearch.toLowerCase())
       );
+    },
+
+      selectContact(event) {
+      
+          let value = event.target.value;
+          console.log(value)
+      this.contacts.forEach((e) => {
+        if (e.id == value) {
+          this.currentContact = e;
+        }
+      });
     },
   },
 
