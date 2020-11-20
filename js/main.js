d7144390522ce050cd24f18885573847eb24a24d
Matteo Fattorini <fattorinimatteo@gmail.com>
2020-11-20 16:30:58 +0100

    Contacts implemented

--- js/main.js
+++ js/main.js
@@ -1,9 +1,56 @@
 const app = new Vue({
-    el: ("#app"),
-    data: {},
-    methods: {},
-    computed: {}
-})
+  el: "#app",
+  data: {
+    mySelf: {
+      avatar: "css/img/avatar_2.jpg",
+      name: "Matteo Fattorini",
+    },
+
+    contacts: [
+      (giulia = {
+        avatar: "css/img/avatar_io.jpg",
+        name: "Noemi",
+        lastSeen: "",
+      }),
+      (mySelf = {
+        avatar: "css/img/avatar_1.jpg",
+        name: "Jacopo",
+      }),
+      (mySelf = {
+        avatar: "css/img/avatar_3.jpg",
+        name: "Giulio",
+        lastSeen: "",
+      }),
+      (mySelf = {
+        avatar: "css/img/avatar_4.jpg",
+        name: "Marco",
+        lastSeen: "",
+      }),
+      (mySelf = {
+        avatar: "css/img/avatar_5.jpg",
+        name: "Elia",
+        lastSeen: "",
+      }),
+      (mySelf = {
+        avatar: "css/img/avatar_6.jpg",
+        name: "Giulia",
+        lastSeen: "",
+      }),
+      (mySelf = {
+        avatar: "css/img/avatar_7.jpg",
+        name: "Alessio",
+        lastSeen: "",
+      }),
+      (mySelf = {
+        avatar: "css/img/avatar_8.jpg",
+        name: "Luca",
+        lastSeen: "",
+      }),
+    ],
+  },
+  methods: {},
+  computed: {},
+});
 
 
 
