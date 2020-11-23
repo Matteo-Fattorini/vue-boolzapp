9846ecbf6b6c336714dd24711fb367e713d77500
Matteo Fattorini <fattorinimatteo@gmail.com>
2020-11-23 14:14:19 +0100

    Revamped messages.
    Messages are now stored in obj, making it easier to work with. 
    New data.js added

---
+++ js/data.js
@@ -0,0 +1,226 @@
+let contacts = [
+  //array of contacts, as objs.
+  {
+    avatar: "css/img/avatar_io.jpg",
+    name: "Noemi",
+    lastSeen: "22:32",
+    messages: [
+      {
+        text: "Hey what's going on?",
+        timeStamp: "22.15",
+        isSent: false,
+      },
+      {
+        text: "Everything's good!",
+        timeStamp: "22.15",
+        isSent: true,
+      },
+      {
+        text: "Wanna hang up later?",
+        timeStamp: "22.18",
+        isSent: false,
+      },
+      {
+        text: "Sure!",
+        timeStamp: "22.19",
+        isSent: true,
+      },
+    ],
+    id: 100,
+  },
+  {
+    avatar: "css/img/avatar_1.jpg",
+    name: "Jacopo",
+    lastSeen: "21:00",
+    messages: [
+      {
+        text: "Hey man do you like this app?",
+        timeStamp: "21.15",
+        isSent: false,
+      },
+      {
+        text: "I love it!",
+        timeStamp: "21.15",
+        isSent: true,
+      },
+      {
+        text: "Do you think they are gonna patch it?",
+        timeStamp: "21.18",
+        isSent: false,
+      },
+      {
+        text: "Not sure about that!!",
+        timeStamp: "21.19",
+        isSent: true,
+      },
+    ],
+    id: 101,
+  },
+  {
+    avatar: "css/img/avatar_3.jpg",
+    name: "Leo",
+    lastSeen: "20:50",
+    messages: [
+      {
+        text: "I heard a super funny joke yesterday, wanna hear it?",
+        timeStamp: "19.15",
+        isSent: false,
+      },
+      {
+        text: "Sure, go on!",
+        timeStamp: "19.15",
+        isSent: true,
+      },
+      {
+        text: "What is the biggest lie in the entire universe? -'I have read and agree to the Terms & Conditions.'",
+        timeStamp: "19.18",
+        isSent: false,
+      },
+      {
+        text: "Ahah!",
+        timeStamp: "19.19",
+        isSent: true,
+      },
+    ],
+    id: 102,
+  },
+  {
+    avatar: "css/img/avatar_4.jpg",
+    name: "Marco",
+    lastSeen: "19:00",
+    messages: [
+      {
+        text: "Did you hear the news?",
+        timeStamp: "18.15",
+        isSent: false,
+      },
+      {
+        text: "Yeah, unbelievable!",
+        timeStamp: "18.15",
+        isSent: true,
+      },
+
+    ],
+    id: 103,
+  },
+  {
+    avatar: "css/img/avatar_5.jpg",
+    name: "Elia",
+    lastSeen: "18:00",
+    messages: [
+      {
+        text: "Hey, you heard what happened to Marco?",
+        timeStamp: "17.15",
+        isSent: false,
+      },
+      {
+        text: "No, what happened?",
+        timeStamp: "17.15",
+        isSent: true,
+      },
+      {
+        text: "He is no longer engaged... ",
+        timeStamp: "17.18",
+        isSent: false,
+      },
+      {
+        text: "Omg!",
+        timeStamp: "17.28",
+        isSent: true,
+      },
+    ],
+    id: 104,
+  },
+  {
+    avatar: "css/img/avatar_6.jpg",
+    name: "Giulia",
+    lastSeen: "16:00",
+    messages: [
+      {
+        text: "Ow, i love kittens so much!",
+        timeStamp: "15.15",
+        isSent: false,
+      },
+      {
+        text: "I actually prefer dogs",
+        timeStamp: "15.15",
+        isSent: true,
+      },
+      {
+        text: "They can't purr!",
+        timeStamp: "15.18",
+        isSent: false,
+      },
+      {
+        text: "But they can fetch!",
+        timeStamp: "15.41",
+        isSent: true,
+      },
+    ],
+    id: 105,
+  },
+  {
+    avatar: "css/img/avatar_7.jpg",
+    name: "Alessio",
+    lastSeen: "14:00",
+    messages: [
+      {
+        text: "Wanna hear a joke?",
+        timeStamp: "13.15",
+        isSent: false,
+      },
+      {
+        text: "Sure..!",
+        timeStamp: "13.15",
+        isSent: true,
+      },
+      {
+        text: "What is the best thing about Switzerland?  I don't know, but the flag is a big plus!",
+        timeStamp: "13.18",
+        isSent: false,
+      },
+      {
+        text: "Ahahah!",
+        timeStamp: "13.36",
+        isSent: true,
+      },
+    ],
+    id: 106,
+  },
+  {
+    avatar: "css/img/avatar_8.jpg",
+    name: "Luca",
+    lastSeen: "12:00",
+    messages: [
+      {
+        text: "Such a rainy day today",
+        timeStamp: "11.15",
+        isSent: false,
+      },
+      {
+        text: "Yeah, we had better days",
+        timeStamp: "11.15",
+        isSent: true,
+      },
+
+    ],
+    id: 107,
+  },
+];
+    
+
+let possibleAnswers = [
+      //possible answer that every contact can give
+      "Not sure about that",
+      "Yeah, whatever",
+      "I guess so...",
+      "Probably not",
+      "Yeah, indeed!",
+      "No, i don't think so",
+      "Are you sure about that?",
+      "I never tought about that!",
+      "I would love to!",
+      "I always wanted you to ask me that...",
+      "Me too!",
+    ]
+
--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@
  * Boolzap: a What's app replica using Vue.js
  */
 
-
 const app = new Vue({
   el: "#app",
   data: {
@@ -11,6 +10,9 @@ const app = new Vue({
       avatar: "css/img/avatar_2.jpg", //self-contact
       name: "Matteo Fattorini",
     },
+    contacts, //get contacts from data.js
+    possibleAnswers, //get answers from data.js
+
     isWriting: false, //will use this to know when the automate answer is triggered
 
     filteredContacts: [], //new array, based of search
@@ -18,124 +20,9 @@ const app = new Vue({
     currentContact: {
       //this will be the selected contacts. Everything will change based on this
     },
+
     contactSearch: "", //v-modeled with search in contact
     sendMessageText: "", //v-modeled with sendMessage
-    possibleAnswers: [
-      //possible answer that every contact can give
-      "Not sure about that",
-      "Yeah, whatever",
-      "I guess so...",
-      "Probably not",
-      "Yeah, indeed!",
-      "No, i don't think so",
-      "Are you sure about that?",
-      "I never tought about that!",
-      "I would love to!",
-      "I always wanted you to ask me that...",
-      "Me too!",
-    ],
-
-    contacts: [
-      //array of contacts, as objs.
-      {
-        avatar: "css/img/avatar_io.jpg",
-        name: "Noemi",
-        lastSeen: "22:32",
-        lastMessage: [
-          "Hey what's going on?",
-          "Everything's good!",
-          "Wanna hang up later?",
-          "Sure!",
-          "Awesome!",
-        ],
-        timeStamp: ["22:15", "22:15", "22:18", "22:19", "22:20"],
-        id: 100,
-      },
-      {
-        avatar: "css/img/avatar_1.jpg",
-        name: "Jacopo",
-        lastSeen: "22:00",
-        lastMessage: [
-          "Have you seen the news?",
-          "Yeah, unbelievable!",
-          "I told you it would be that way!",
-          "Guess you were right!",
-          "I always am!",
-        ],
-        timeStamp: ["22:12", "22:12", "22:13", "22:13", "22:14"],
-        id: 101,
-      },
-      {
-        avatar: "css/img/avatar_3.jpg",
-        name: "Leo",
-        lastSeen: "21:50",
-        lastMessage: ["So tomorrow at 10.00?", "Sure!", "Great!"],
-        id: 102,
-        timeStamp: ["21:39", "21:40", "21:45"],
-      },
-      {
-        avatar: "css/img/avatar_4.jpg",
-        name: "Marco",
-        lastSeen: "20:00",
-        lastMessage: [
-          "What do you think about this new Whats'app?",
-          "I love it!",
-          "Yeah me too! I heard next patch will be emoticon!",
-          "Yeah sure...!",
-          "I got that from reliable sources!",
-        ],
-        timeStamp: ["19:32", "19:33", "19:33", "19:34", "19:35"],
-        id: 103,
-      },
-      {
-        avatar: "css/img/avatar_5.jpg",
-        name: "Elia",
-        lastSeen: "19:00",
-        lastMessage: [
-          "Man, we should hang up sometime",
-          "Tomorrow?",
-          "Sounds Good!",
-        ],
-        timeStamp: ["18:32", "18:33", "18:33"],
-        id: 104,
-      },
-      {
-        avatar: "css/img/avatar_6.jpg",
-        name: "Giulia",
-        lastSeen: "18:00",
-        lastMessage: ["Going to sleep now, see you tomorrow!", "Good night!"],
-        timeStamp: ["17:32", "17:33"],
-        id: 105,
-      },
-      {
-        avatar: "css/img/avatar_7.jpg",
-        name: "Alessio",
-        lastSeen: "17:00",
-        lastMessage: [
-          "Yeah now it works!",
-          "Do you like this app?",
-          "It's pretty good!",
-          "Yeah i love it too!",
-          "Why some button's aren't working?",
-          "Yeah i have to go now, bye!",
-        ],
-        timeStamp: ["16:32", "16:33", "16:33", "16:34", "16:35", "16:36"],
-        id: 106,
-      },
-      {
-        avatar: "css/img/avatar_8.jpg",
-        name: "Luca",
-        lastSeen: "12:00",
-        lastMessage: [
-          "Do you like hot dogs?",
-          "I like them a lot",
-          "How about one right now",
-          "Sure!",
-        ],
-        id: 107,
-        timeStamp: ["11:32", "11:33", "11:33", "11:34"],
-      },
-    ],
   },
 
   methods: {
@@ -149,7 +36,7 @@ const app = new Vue({
     },
 
     //this function keep the scroll to the last sent element. Will be called on updated() section.
-    
+
     scrollToElement() {
       let len = this.$refs.listItem.length;
       let al = this.$refs.listItem[len - 1]; //gets all items with the class "listItem", specifically the last one
@@ -172,14 +59,18 @@ const app = new Vue({
       let answer = this.possibleAnswers;
       let random = Math.floor(Math.random() * maxLen);
       let current = this.currentContact;
-      if (this.sendMessageText.length > 0 && !this.isWriting) {
-        current.lastMessage.push(this.sendMessageText); //this handles the message: will push a new timeStamp ad a new string
-        current.timeStamp.push(time); // to current contact respective elements.
+      if (this.sendMessageText.length > 0) {
+        //this handles the message: will push a new timeStamp ad a new string
+        current.messages.push({
+          text: this.sendMessageText,
+          timeStamp: time,
+          isSent: true,
+        });
+
         this.filteredContacts.forEach((e) => (e.writtenTo = false));
         current.writtenTo = true; /** writtenTo = true to the contact last written to, everyone else is on false. */
         this.filteredContacts.sort(
-          (a, b) =>
-            a.writtenTo > b.writtenTo ? -1 : 1
+          (a, b) => (a.writtenTo > b.writtenTo ? -1 : 1)
           // This variable is used to sort the array, based on the person
           // who has writtenTo = true
         );
@@ -203,8 +94,11 @@ const app = new Vue({
       app.isWriting = true;
       setTimeout(function () {
         current.lastSeen = time;
-        current.lastMessage.push(answers[random]);
-        current.timeStamp.push(time);
+        current.messages.push({
+          text: answers[random],
+          timeStamp: time,
+          isSent: false,
+        });
         app.isWriting = false;
       }, 2000);
     },
